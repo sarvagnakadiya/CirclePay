7e010badb7f87efc444dfbf79529a4ed63a1d2a7
sarvagnakadiya <sarvagnakadiya1@gmail.com>
2024-12-13 01:03:17 +0530

    wallet disconnect error solved

--- src/app/utils/config.tsx
+++ src/app/utils/config.tsx
@@ -1,4 +1,4 @@
-import { http, createConfig } from "@wagmi/core";
+import { getDefaultConfig } from "@rainbow-me/rainbowkit";
 import {
   mainnet,
   arbitrum,
@@ -7,39 +7,10 @@ import {
   arbitrumSepolia,
   baseSepolia,
 } from "@wagmi/core/chains";
-import { type Chain } from "viem";
-export const local = {
-  id: 1,
-  name: "Ethereum",
-  nativeCurrency: { name: "Ether", symbol: "ETH", decimals: 18 },
-  rpcUrls: {
-    default: { http: ["http://127.0.0.1:8545"] },
-  },
-  blockExplorers: {
-    default: { name: "Etherscan", url: "https://etherscan.io" },
-  },
-} as const satisfies Chain;
-import {
-  injected,
-  coinbaseWallet,
-  metaMask,
-  mock,
-  safe,
-} from "@wagmi/connectors";
 
-export const config = createConfig({
+export const config = getDefaultConfig({
   chains: [mainnet, arbitrum, optimism, polygon, arbitrumSepolia, baseSepolia],
-  // connectors: [injected(), coinbaseWallet(), metaMask(), safe()],
-  connectors: [injected()],
-  transports: {
-    [mainnet.id]: http(),
-    [arbitrum.id]: http(),
-    [optimism.id]: http(),
-    [polygon.id]: http(),
-    [arbitrumSepolia.id]: http(),
-    [baseSepolia.id]: http(),
-
-    // [local.id]: http("http://127.0.0.1:8545"),
-  },
-  // ssr: true,
+  appName: "RainbowKit demo",
+  projectId: "8a002f09d4fc6fba7c4cd6d06df5e19f",
+  ssr: true,
 });
